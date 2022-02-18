5e6d9115f5f4f575fff543a9aeaeaca548380b4e
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-18 20:18:16 -0300

    :wrench: chore: category coupling in project

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,5 +1,3 @@
-
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -7,4 +5,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('category/', include('category.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
