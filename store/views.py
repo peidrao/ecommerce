583d3dc1509c900e8a0d078162089ea60d9bb0d1
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-19 09:59:39 -0300

    :sparkles: chore: page store and category

--- store/views.py
+++ store/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from category.models import Category
+
+from store.models import Product
 
 # Create your views here.
+
+
+def store(request, category_slug: str = None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(
+            category=categories, is_available=True)
+        products_count = products.count()
+    else:
+        products = Product.objects.filter(is_available=True)
+        products_count = products.count()
+    
+
+    context = {
+        'products': products,
+        'count': products_count
+    }
+    return render(request, 'store/store.html', context)
