e914845e0db10ece1934ee57c65f54aa4e5c40dc
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-19 08:57:01 -0300

    :sparkles: chore: add home template

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,8 +3,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('category/', include('category.urls')),
     path('account/', include('accounts.urls')),
+    path('store/', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
