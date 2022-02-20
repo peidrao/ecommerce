da3d3e3300b3b40f72e9179261d6ce9091b41393
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-20 17:04:14 -0300

    :sparkles: feat: product to added in cart

--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404, render
+from carts.models import CartItem
 from category.models import Category
 
 from store.models import Product
 
+from carts.views import _cart_id
 # Create your views here.
 
 
@@ -30,10 +32,13 @@ def product_detail(request, category_slug: str = None, product_slug: str = None)
     try:
         product = Product.objects.get(
             category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(
+            cart__cart_id=_cart_id(request), product=product).exists()
     except Product.DoesNotExist:
         product = None
 
     context = {
-        'product': product
+        'product': product,
+        'in_cart': in_cart
     }
     return render(request, 'store/product_detail.html', context)
