4b65da927eec912ffebbfe15b36936e61ee0ee88
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-23 20:02:43 -0300

    :bug: fix: add size in cart

--- carts/views.py
+++ carts/views.py
@@ -33,11 +33,20 @@ def add_cart(request, product_id):
 
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            for item in product_variation:
+                cart_item.variations.add(item)
         cart_item.quantity += 1
         cart_item.save()
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(
             product=product, quantity=1, cart=cart)
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            for item in product_variation:
+                cart_item.variations.add(item)
+
         cart_item.save()
 
     return redirect('cart')
