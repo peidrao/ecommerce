b203dd3a42332764056fe33831a467e0446d8e20
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-25 19:53:57 -0300

    :sparkles: feat: add pages and form registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import RegistartionForm
 
-# Create your views here.
+def register(request):
+    context = {
+        'form': RegistartionForm()
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    return render(request, 'accounts/login.html', {})
+
+def logout(request):
+    pass
