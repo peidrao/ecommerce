e3316ab57ccd90e402fa97cdae4a5e6af4f11e49
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-25 20:24:45 -0300

    :sparkles: feat: custom registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,8 +2,33 @@ from django import forms
 from .models import Account
 
 
-class RegistartionForm(forms.ModelForm):
-
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}))
+ 
     class Meta:
         model = Account
         fields = ('first_name', 'last_name', 'phone_number', 'email', 'password')
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        password2= cleaned_data.get('password2')
+
+        if password != password2:
+            raise forms.ValidationError('Password does not match')
+
+
+        return super().clean()
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+       
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Your First name' 
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Your Last name' 
+        self.fields['email'].widget.attrs['placeholder'] = 'Your Email' 
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Your Phone Number' 
+  
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,53 @@
-from django.shortcuts import render
-from .forms import RegistartionForm
+from django.shortcuts import render, redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+from django.utils.encoding import force_bytes
+
+from .forms import RegistrationForm
+from .models import Account
+
 
 def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+
+            # Create a user profile
+            profile = UserProfile()
+            profile.user_id = user.id
+            profile.profile_picture = 'default/default-user.png'
+            profile.save()
+
+            # USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            # messages.success(request, 'Thank you for registering with us. We have sent you a verification email to your email address [rathan.kumar@gmail.com]. Please verify it.')
+            return redirect('/accounts/login/?command=verification&email='+email)
+    else:
+        form = RegistrationForm()
     context = {
-        'form': RegistartionForm()
+        'form': form,
     }
     return render(request, 'accounts/register.html', context)
 
