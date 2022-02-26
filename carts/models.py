9149827abc421a8094ca60502d2876bed3fc5a0a
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-26 19:58:12 -0300

    :sparkles: feat: add user in cart item

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from store.models import Product, Variation
+from accounts.models import Account
 
 
 class Cart(models.Model):
@@ -11,6 +12,7 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     variations = models.ManyToManyField(Variation, blank=True)
     product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
     cart = models.ForeignKey(Cart, on_delete=models.DO_NOTHING)
