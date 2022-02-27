80a3945d23f34bf1b990b53033f68fd08a134f10
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-27 11:09:01 -0300

    :sparkles: feat: models and migrations orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order, OrderProduct, Payment
+
+
+admin.site.register(Order)
+admin.site.register(OrderProduct)
+admin.site.register(Payment)
