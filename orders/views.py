655a043c2aad2e992f3f33f257eeeacde50b426a
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-27 11:14:28 -0300

    :sparkles: feat: init orders views

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import json
+import datetime
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
+from carts.models import CartItem
+from store.models import Product
+
+from .models import Order, Payment, OrderProduct
+from .forms import OrderForm
+
+def payments(request):
+    pass
+
+
+def place_order(request, total=0, quantity=0):
+    pass
+
+def order_complete(request):
+    pass
