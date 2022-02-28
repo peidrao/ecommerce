53e70e34dec74b099a2b27103784f849b46d0793
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-28 19:53:24 -0300

    :sparkles: feat: add review rating model

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Product, Variation
+from .models import Product, ReviewRating, Variation
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -15,3 +15,4 @@ class VariationAdmin(admin.ModelAdmin):
 
 admin.site.register(Variation, VariationAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ReviewRating)
